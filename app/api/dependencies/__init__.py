ea9e9841ee6356474a010a8907ceaaf194fe3cba
HELAFUG <loxand42@gmail.com>
2025-12-03 17:41:23 +0300

    add access token

---
+++ app/api/dependencies/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ("get_access_tokens_db",)
+
+from .access_tokens import get_access_tokens_db
---
+++ app/api/dependencies/access_tokens.py
@@ -0,0 +1,14 @@
+from typing import TYPE_CHECKING
+from fastapi import Depends
+
+from core.models import AccessToken
+from core.helpers import db_helper
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def get_access_tokens_db(
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    yield AccessToken.get_db(session)
