a6724fd1dbbcf5d7c42d2fa37f99356c1dcb2b95
HELAFUG <loxand42@gmail.com>
2025-12-03 18:08:06 +0300

    made database strategy

--- app/api/dependencies/authentication/__init__.py
+++ app/api/dependencies/authentication/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "get_access_tokens_db",
     "get_users_db",
     "get_user_manager",
+    "get_database_strategy",
 )
 
 from .access_tokens import get_access_tokens_db
 from .users import get_users_db
 from .user_manager import get_user_manager
+from .strategy import get_database_strategy
