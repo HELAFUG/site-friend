04b22df7780dfe9f7b2fec734aeff54b42fff994
HELAFUG <loxand42@gmail.com>
2025-12-03 17:34:12 +0300

    add param for jwt

--- app/core/config.py
+++ app/core/config.py
@@ -41,10 +41,17 @@ class APIConfig(BaseModel):
         return path.removeprefix("/")
 
 
+class AccessToken(BaseModel):
+    lifetime_seconds: int = 3600
+    reset_password_token_secret: str = getenv("RESET_PASSWORD_TOKEN_SECRET")
+    verification_token_secret: str = getenv("VERIFICATION_TOKEN_SECRET")
+
+
 class Settings(BaseSettings):
     db: DBConfig = DBConfig()
     srv: SRVConfig = SRVConfig()
     api: APIConfig = APIConfig()
+    access_token: AccessToken = AccessToken()
 
 
 settings = Settings()
