59df5f312cb5b7dbb45dd7f6fe7bd5a1f766aae0
HELAFUG <loxand42@gmail.com>
2025-12-03 17:47:24 +0300

    made get_users_db method

---
+++ app/api/dependencies/users.py
@@ -0,0 +1,15 @@
+from typing import Annotated, TYPE_CHECKING
+from fastapi import Depends
+
+from core.models import User
+from core.helpers import db_helper
+
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def get_users_db(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    yield User.get_db(session)
