1ba90c56cae1de99ea3b6301150c03a7c83e52c8
HELAFUG <loxand42@gmail.com>
2025-12-03 18:07:06 +0300

    made a user manager dependency

--- app/api/dependencies/authentication/__init__.py
+++ app/api/dependencies/authentication/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "get_access_tokens_db",
     "get_users_db",
+    "get_user_manager",
 )
 
 from .access_tokens import get_access_tokens_db
 from .users import get_users_db
+from .user_manager import get_user_manager
