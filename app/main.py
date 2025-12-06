cdb34aefa8b35557726dca616f90dc0a90bc0505
HELAFUG <loxand42@gmail.com>
2025-12-06 14:24:47 +0300

    add cors middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from core.helpers import db_helper
 from core.config import settings
 from core import broker
@@ -19,6 +20,14 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
