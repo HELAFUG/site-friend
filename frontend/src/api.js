06870cf6ea4d28dbb221c1f8e53049fb4f96c072
HELAFUG <loxand42@gmail.com>
2025-12-06 13:21:42 +0300

    add api js

---
+++ frontend/src/api.js
@@ -0,0 +1,15 @@
+// src/api.js
+
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8020/api/v1/auth';
+
+export const registerUser = async (data) => {
+    const response = await axios.post(`${API_URL}/register`, data);
+    return response.data;
+};
+
+export const loginUser = async (data) => {
+    const response = await axios.post(`${API_URL}/login`, data);
+    return response.data;
+};
